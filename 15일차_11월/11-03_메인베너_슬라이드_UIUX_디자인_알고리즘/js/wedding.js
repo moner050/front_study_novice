5365ed61756a421b9efdb2302faa290306336ced
MinHyeong <minhyung0237@gmail.com>
2021-11-03 22:09:29 +0900

    2021-11-03 프론트앤드 학습

--- 15일차_11월/11-03_메인베너_슬라이드_UIUX_디자인_알고리즘/js/wedding.js
+++ 15일차_11월/11-03_메인베너_슬라이드_UIUX_디자인_알고리즘/js/wedding.js
@@ -6,14 +6,31 @@
     // 홈페이지 로딩 시 3초 후에 다음 슬라이드 이동
     // 3초간격으로 자동으로 우측에서 좌측으로 화면너비(1903px)의 길이만큼 이동하는 슬라이드
 
+    // // 1. 메인 슬라이드 함수
+    // function mainSlide()
+    // {
+    //     // alert(cnt);
+    //     $('.slide-wrap').animate({left:-1903*cnt},1000, function(){
+    //         if(cnt>2){cnt = 0}
+    //         $('.slide-wrap').animate({left:-1903*cnt}, 0); // 눈속임 리턴
+    //     });
+    // }
+    // // 2. 다음(Next) 카운트 함수
+    // function nextCount()
+    // {
+    //     cnt++;
+    //     mainSlide();
+    // }
+    // // 3. 타이머 호출 
+    // setInterval(nextCount, 3000);
+
     // 1. 메인 슬라이드 함수
     function mainSlide()
     {
-        // alert(cnt);
         $('.slide-wrap').animate({left:-1903*cnt},1000, function(){
-            if(cnt>2){cnt = 0}
-            $('.slide-wrap').animate({left:-1903*cnt}, 0); // 눈속임 리턴
-        });
+            if(cnt > 2){cnt=0}
+            $('.slide-wrap').animate({left:-1903*cnt}, 0);
+        })
     }
     // 2. 다음(Next) 카운트 함수
     function nextCount()
@@ -23,6 +40,4 @@
     }
     // 3. 타이머 호출 
     setInterval(nextCount, 3000);
-
-
 })(jQuery);
