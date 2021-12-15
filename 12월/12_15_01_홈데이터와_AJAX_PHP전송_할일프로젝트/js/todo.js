e2078eb1fa0bd0af7d801b7a65720b4db8f88304
MinHyeong <minhyung0237@gmail.com>
2021-12-15 22:11:41 +0900

    2021-12-15 프론트앤드 학습

--- 12월/12_15_01_홈데이터와_AJAX_PHP전송_할일프로젝트/js/todo.js
+++ 12월/12_15_01_홈데이터와_AJAX_PHP전송_할일프로젝트/js/todo.js
@@ -1,4 +1,70 @@
 (function($)
 {
-    console.log($);
+    var delItem = null;
+    $('.submit-btn').on({
+        click: function(event)
+        {
+            event.preventDefault();
+            var fromSubject = $('#subject').val();
+            var fromContent = $('#content').val();
+            console.log()
+            $.ajax({
+                url: './response.php',
+                type: 'POST',
+                data:{
+                    subject: fromSubject,
+                    content: fromContent
+                },
+                success: function(result)
+                {
+                    var jsonData = JSON.parse(result);
+                    localStorage.setItem(jsonData.제목, jsonData.내용);
+                    // 할일 목록 출력 함수 호출
+                    list();
+                },
+                error: function(err)
+                {
+                    console.log('AJAX 오류!!',err);
+                }
+            });
+        }
+    });
+    function list()
+    {
+        var keySubject = null;
+        var keyContent = null;
+        var txt = '';           // 출력정보 저장
+
+        // 키, 키값
+        for(i = 0; i <localStorage.length; i++)
+        {
+            keySubject = localStorage.key(i);
+            keyContent = localStorage.getItem(localStorage.key(i));
+            txt += "<tr>";
+            txt += '<td><input type="checkbox" class="chk" value="'+ keySubject +'"></td>';
+            txt += '<td><p>'+ keySubject +'</p></td>';
+            txt += '<td><p>'+ keyContent +'</p></td>';
+            txt += "</tr>";
+        } 
+        // 바인딩
+        $('#listBox tbody').html(txt);
+    }
+    // 할일 목록 출력 함수 호출
+    list();
+
+    // 체크상자 클릭 이벤트
+    $(document).on('click', '.chk', function()
+    {
+        delItem = $(this).val();
+    });
+    
+    // 삭제버튼 클릭 이벤트
+    $('.delete-btn').on({
+        click: function()
+        {
+            localStorage.removeItem(delItem);
+            list();
+        }
+    });
+
 })(jQuery);
--- 12월/12_15_01_홈데이터와_AJAX_PHP전송_할일프로젝트/response.php
+++ 12월/12_15_01_홈데이터와_AJAX_PHP전송_할일프로젝트/response.php
@@ -1,29 +1,24 @@
 <?
 
-$irum = $_POST["irum"];
-$tel = $_POST["tel"];
+$subject = $_POST["subject"];
+$content = $_POST["content"];
 
-// 데이터베이스 사용권한(계정)
-// 데이터베이스 저장(input)
-// 데이터베이스 가져오기(select)
+//db
 
-// 응답
-// echo $irum."님 성공적으로 저장되었습니다.";
 
 // $array = Array() 배열 생성자 함수
 $array = Array(
-    "이름" => $irum,
-    "연락처" => $tel,
-    "소속" => $job
+    "제목" => $subject,
+    "내용" => $content
 ); 
 // JSON 데이터 형식 인코딩 Encode : 데이터 형식을 변환 한다.
-$json = json_encode($array, JSON_UNESCAPED_UNICODE);
+$response = json_encode($array, JSON_UNESCAPED_UNICODE);
 
 // JSON 파일 내보내기(저장)
-// 파일명: data폴더 안에 저장 : member.json
-$file = file_put_contents('./data/member.json', $json);
+// 파일명: data폴더 안에 저장 : todo.json
+$savefile = file_put_contents('./data/todo.json', $response);
 
 // JSON 데이터로 응답
-echo $json;
+echo $response;
 
 ?>
