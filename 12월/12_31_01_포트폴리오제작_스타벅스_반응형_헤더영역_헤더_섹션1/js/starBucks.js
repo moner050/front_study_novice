7c4f22d89250df9d8ab264e09faea18dacf38f77
moner050 <83222976+moner050@users.noreply.github.com>
2022-01-01 00:02:10 +0900

    2021-12-31 프론트앤드 학습

--- 12월/12_31_01_포트폴리오제작_스타벅스_반응형_헤더영역_헤더_섹션1/js/starBucks.js
+++ 12월/12_31_01_포트폴리오제작_스타벅스_반응형_헤더영역_헤더_섹션1/js/starBucks.js
@@ -60,7 +60,18 @@
                 // 1 === "1" false
                 if( $('#aside').hasClass('addSearch') )
                 {
-                    alert('클래스 추가된 상태. 검색할 내용을 입력상자에 입력하세요.');
+                    // alert('클래스 추가된 상태. 검색할 내용을 입력상자에 입력하세요.');
+                    if( $('#search').val() == "" )
+                    {// 입력 상자에 입력 내용이 없다면
+                        alert('입력상자에 검색할 내용이 없습니다.');
+                    }
+                    else
+                    {
+                        alert('전송 완료');
+                        // search.submit();
+                        // AJAX 전송 PHP파일
+
+                    }
                 }
                 else
                 {  
@@ -75,7 +86,21 @@
 
     section1()
     {
-    
+        function anifn()
+        {
+            $('.ani1').stop().animate({opacity:1},500, function(){
+                $('.ani2').stop().animate({opacity:1},500, function(){
+                    $('.ani3').stop().animate({opacity:1},500, function(){
+                        $('.ani4').stop().animate({opacity:1},500, function(){
+                            $('.ani5').stop().animate({opacity:1},500,);
+                        });
+                    });
+                });
+            });
+        }
+        setTimeout(anifn, 800);
+
+        
     },
     section2()
     {
