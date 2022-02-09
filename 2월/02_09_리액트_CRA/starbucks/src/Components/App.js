d810ba21f2e5c4bc32c836ed8f0e8a7db6c715cd
moner050 <minhyung0237@gmail.com>
2022-02-09 21:47:49 +0900

    2022-02-09 스타벅스 리액트 프로젝트

---
+++ 2월/02_09_리액트_CRA/starbucks/src/Components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import SkipComponent from "./SkipComponent";
+import WrapComponent from "./WrapComponent";
+import MobieNavComponent from "./MobieNavComponent";
+import QuickMenuComponent from "./QuickMenuComponent";
+import GoTopComponent from "./GoTopComponent";
+import ValidatorComponent from "./ValidatorComponent";
+
+
+function App() {
+  return (
+    
+    <React.Fragment>
+      <SkipComponent />
+      <WrapComponent />
+      <MobieNavComponent />
+      <QuickMenuComponent />
+      <GoTopComponent />
+      <ValidatorComponent />  
+    </React.Fragment>
+  
+  );
+}
+
+export default App;
