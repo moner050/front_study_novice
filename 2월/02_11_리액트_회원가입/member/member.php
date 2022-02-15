0a962ae8519ad20afe2d054f75f3e798e7aca030
moner050 <83222976+moner050@users.noreply.github.com>
2022-02-15 19:48:06 +0900

    보안문제

--- 2월/02_11_리액트_회원가입/member/member.php
+++ 2월/02_11_리액트_회원가입/member/member.php
@@ -3,7 +3,7 @@
     // 데이터베이스 헤더영역 - 데이터베이스 이름, 비밀번호, (인증) 데이터베이스 열기
     $serverName = 'localhost';
     $userName = 'moner050';
-    $userPwd = 'Sjsmsanjfk0502!';
+    $userPwd = 'qwer1234';
     $dbName = 'moner050';
 
     $connector = mysqli_connect($serverName,$userName,$userPwd,$dbName);
