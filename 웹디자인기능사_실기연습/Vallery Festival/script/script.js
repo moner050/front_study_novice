5f2abdc9123cbc723875b20d5ec8d9b53a75f754
moner050 <83222976+moner050@users.noreply.github.com>
2022-03-24 21:48:33 +0900

    웹디자인기능사 실기 Vallery Festival 완성

---
+++ 웹디자인기능사_실기연습/Vallery Festival/script/script.js
@@ -0,0 +1,63 @@
+(function($)
+{
+    var vallery = {
+        init: function()
+        {
+            this.header();
+            this.slide();
+            this.popup();
+        },
+        header: function()
+        {
+            $('.main-menu').on({
+                mouseenter: function()
+                {
+                    $(this).find('.sub').stop().fadeIn(200);
+                },
+                focusin: function()
+                {
+                    $(this).find('.sub').stop().fadeIn(200);
+                }
+            });
+            $('.main-menu').on({
+                mouseleave: function()
+                {
+                    $(this).find('.sub').stop().fadeOut(100);
+                },
+                focusout: function()
+                {
+                    $(this).find('.sub').stop().fadeOut(100);
+                }
+            })
+        },
+        slide: function()
+        { 
+            var slideWrap = $('.slide-wrap');
+
+            setInterval(function()
+            {
+                slideWrap.stop().animate({'marginLeft' : '-100%'}, 700, function()
+                {
+                    $('.slide:first').appendTo(slideWrap);
+                    $(slideWrap).css({'marginLeft' : '0'})
+                })
+            }, 3000)
+        },
+        popup: function()
+        {
+            $('.notice li:nth-child(1)').on({
+                click: function()
+                {
+                    $('#popUpBox').show();
+                }
+            });
+            $('.popUp button').on({
+                click: function()
+                {
+                    $('#popUpBox').hide();
+                }
+            })
+        }
+    }
+    vallery.init();
+})(jQuery);
