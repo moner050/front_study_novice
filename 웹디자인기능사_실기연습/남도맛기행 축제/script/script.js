1a010a8f4525dd18ff5f85ca561cca25d9621419
moner050 <83222976+moner050@users.noreply.github.com>
2022-03-24 16:17:30 +0900

    웹디자인기능사 실기 남도맛기행 축제 완성

---
+++ 웹디자인기능사_실기연습/남도맛기행 축제/script/script.js
@@ -0,0 +1,55 @@
+(function($)
+{
+    var namdo = {
+        init: function()
+        {
+            this.header();
+            this.slide();
+            this.popup();
+        },
+        header: function()
+        {
+            $('.main-menu').on({
+                mouseenter: function()
+                {
+                    $(this).find('.sub').stop().fadeIn(100);
+                }
+            });
+            $('.main-menu').on({
+                mouseleave: function()
+                {
+                    $(this).find('.sub').stop().fadeOut(100);
+                }
+            });
+        },
+        slide: function()
+        {
+            var slideWrap = $('.slide-wrap');
+
+            setInterval(function()
+            {
+                slideWrap.stop().animate({'marginLeft' : '-100%'}, 700, function()
+                {
+                    $('.slide:first').appendTo(slideWrap);
+                    $(slideWrap).css({'marginLeft' : '0'})
+                })
+            }, 3000)
+        },
+        popup: function()
+        {
+            $('.notice li:nth-child(1)').on({
+                click: function()
+                {
+                    $('.popUpBox').show();
+                }
+            });
+            $('.popUp button').on({
+                click: function()
+                {
+                    $('.popUpBox').hide();
+                }
+            })
+        }
+    }
+    namdo.init();
+})(jQuery);
